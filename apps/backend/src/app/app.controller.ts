6532ee7cc5116bf57faf105a81c2927eadb5f954
ChromeGG <adam@tkaczyk.dev>
2022-01-29 21:29:18 +0100

    add half logic of joining to room implementation

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, ) {}
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log('~ message', message)
+    this.server.emit('message', message);
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    console.log(data)
+    return data;
+  }
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    // console.log(await this.server.fetchSockets())
+    console.log(`Client connected: ${client.id}`);
+   }
 
   @Get()
   getData() {
@@ -14,5 +38,13 @@ export class AppController {
   @Get('/games/create')
   createGame() {
     return this.appService.createGame();
+  }
+
+  @Post('/games/join')
+  joinToGame(@Body() {gameId, userName}: {gameId: string, userName: string}) {
+    console.log(gameId)
+    // ! TODO server is null, fix it
+    this.server.emit('userJoinedToGame', userName);
+    return gameId
   }
 }
--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,13 +1,15 @@
-import React, {useState, useEffect, useRef } from 'react'
-import {Button, Grid, Typography, Divider} from '@mui/material';
-import io, {Socket} from "socket.io-client";
+import React, { useState, useEffect, useRef } from 'react';
+import { Button, Grid, Typography, Divider, TextField } from '@mui/material';
+import io, { Socket } from 'socket.io-client';
 import Script from 'next/script';
 import axios from 'axios';
 
 export function Index() {
   // const [socket, setSocket] = useState<Socket>(null);
   const [gameId, setGameId] = useState('');
-  const [users, setUsers] = useState([])
+  const [insertedGameId, setInsertedGameId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [users, setUsers] = useState([]);
 
   // useEffect(() => {
   //   const newSocket = io(`http://${window.location.hostname}:3333`);
@@ -16,25 +18,49 @@ export function Index() {
   //   return () => newSocket.close();
   // }, [setSocket]);
 
-  const createLobby = async  () => {
-    const {data} = await axios(`http://${window.location.hostname}:3333/api/games/create`)
-    setGameId(data.id)
+  const createGame = async () => {
+    const { data } = await axios(`http://localhost:3333/api/games/create`);
+    setGameId(data.id);
     // socket.emit('message', {a:1})
     // socket
-  }
+  };
+
+  const joinToGame = async () => {
+    const {data} = await axios.post(`http://localhost:3333/api/games/join`, {
+      gameId: insertedGameId,
+      userName,
+    })
+    console.log(data);
+    // setGameId(data.id)
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInsertedGameId(event.target.value);
+  };
+  const handleUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
 
   // !! NEXT: See example with rooms https://www.youtube.com/watch?v=ZKEqqIO7n-k
 
   return (
     <div>
       <Typography variant="h4">Familiada</Typography>
-      <Button onClick={createLobby}>Stwórz lobby</Button>
+      <Button onClick={createGame} variant="contained">
+        Stwórz lobby
+      </Button>
       <div>GameId: {gameId}</div>
-      <Divider />
+      {gameId && (
+        <>
+          <TextField value={insertedGameId} label="Game ID" onChange={handleChange} />
+          <TextField value={userName} label="user name" onChange={handleUserNameChange} />
+          <Button variant="contained" onClick={() => joinToGame()}>JOIN</Button>
+        </>
+      )}
       <Typography>List of users in Game:</Typography>
       {users}
     </div>
-  )
+  );
 }
 
 export default Index;

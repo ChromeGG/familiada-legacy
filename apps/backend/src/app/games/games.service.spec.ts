aeea96d04de17881dfd93744d8bc8de65ca0da1e
ChromeGG <adam@tkaczyk.dev>
2022-04-23 18:46:17 +0200

    Run first test

--- apps/backend/src/app/games/games.service.spec.ts
+++ apps/backend/src/app/games/games.service.spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { GamesService } from './games.service'
+// import { Test, TestingModule } from '@nestjs/testing'
+// import { GamesService } from './games.service'
 
 describe('GamesService', () => {
-  let service: GamesService
+  // let service: GamesService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GamesService],
-    }).compile()
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [GamesService],
+  //   }).compile()
 
-    service = module.get<GamesService>(GamesService)
+  //   service = module.get<GamesService>(GamesService)
+  // })
+
+  // it('should be defined', () => {
+  //   expect(service).toBeDefined()
+  // })
+
+  describe('create', () => {
+    it('should create a game', () => {
+      console.log(process.env.NODE_ENV)
+      console.log(process.env.REDIS_PORT)
+      // ! NEXT: sprawdzić jak to odpalić
+      expect(true).toBe(true)
+    })
   })
-
-  it('should be defined', () => {
-    expect(service).toBeDefined()
-  })
+  // it.todo('should create a game')
 })
